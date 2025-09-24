a0955db33f8012a3fe9a8ee431c8b637fe32b0d4
Brody Von Berge <brodyvonberge@MAC8248.lc.luther.edu>
2025-09-23 22:37:30 -0500

    upload calculator project

--- notes/04-fruittime/main.js
+++ notes/04-fruittime/main.js
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 function saveReview(){
-    let fruitName = document.querySelectory("#fruitName").value;
+    let fruitName = document.querySelector("#fruitName").value;
     let fruitReview = document.querySelector("#fruitReview").value;
     let fruitRating = "";
     if (fruitName == ""){
@@ -36,41 +35,28 @@ function saveReview(){
     allReviews.appendChild(newReview);
     document.querySelector(newReview).value=""; ///this was the second attempt to clear after submission
     fruitName.innerHTML=""; ///do this with all input areas to clear on submit
-=======
-function saveReview() {
-  let fruitName = document.querySelector("#fruitName").value;
-  let fruiReview = document.querySelector("#fruitReview").value;
-  let fruitRating = "";
-  if (fruitName === "") {
-    return;
-  }
-  try {
-    fruitRating = parseInt(
-      document.querySelector("input[name='rating']:checked").value
-    );
-  } catch (e) {
-    return;
-  }
-  let newReview = document.createElement("p");
-  newReview.classList.add("box");
-  newReview.innerHTML = `<strong>${fruitName}</strong><br>${fruiReview}`;
 
-  switch (fruitRating) {
-    case 1:
-      newReview.classList.add("has-background-danger");
-      break;
-    case 2:
-      newReview.classList.add("has-background-warning-light");
-      break;
-    case 3:
-      newReview.classList.add("has-background-info");
-      break;
-    case 4:
-      newReview.classList.add("has-background-success");
-      break;
+    let all_reviews = localStorage.getItem("all_reviews");
+    if (all_reviews){
+      all_reviews = JSON.parse(all_reviews);
+    } else {
+      all_reviews = [];
+    }
+    let newReviewItem = {};
+    newReviewIem = fruitName;
+    newReviewItem.rating = fruitRating;
+    newReviewItem.text = fruitReview;
+    all_reviews.push(newReviewItem);
+    localStorage.setItem("all_reviews", JSON.stringify(all_reviews));
+
+}
+
+function loadReviews(){
+  let all_reviews = localStorage.getItem("all_reviews");
+  all_reviews = all_reviews ?JSON.parse(all_reviews) : [];
+
+  let allReviewsElement = document.querySelector("#reviewsDiv");
+  for (let review of all_reviews){
+    
   }
-  let allReviews = document.querySelector("#reviewsDiv");
-  allReviews.appendChild(newReview);
-  document.querySelector("#fruitName").value = "";
->>>>>>> bec812f1035e3fd70ddc9fc720e83eb2a8832196
 }
--- projects/calculator/js/main.js
+++ projects/calculator/js/main.js
@@ -11,19 +11,37 @@ var clearOnEntry;
  * 
  * @param {number} digit digit to add or display on the `outputScreen`
  */
-function enterDigit(digit) {}
+function enterDigit(digit) {
+    if (clearOnEntry) {
+        outputScreen.textContent = "";
+        clearOnEntry = false;
+    }
+
+    outputScreen.textContent += digit;
+}
 
 
 /**
  * Clear `outputScreen` and set value to 0
  */
-function clear_screen() {}
+function clear_screen() {
+    outputScreen.textContent = "0";
+    clearOnEntry = true;
+}
 
 
 /**
  * Evaluate the expression and display its result or *ERROR*
  */
-function eval_expr() {}
+function eval_expr() {
+    try {
+        let result = eval(outputScreen.textContent);
+        outputScreen.textContent = result;
+    } catch (e) {
+        outputScreen.textContent = "ERROR";
+    }
+    clearOnEntry = true;
+}
 
 
 /**
@@ -31,10 +49,19 @@ function eval_expr() {}
  * 
  * @param {string} operation to add to the expression
  */
-function enterOp(operation) {}
+function enterOp(operation) {
+    if (clearOnEntry) {
+        clearOnEntry = false;
+    }
+
+    let lastChar = outputScreen.textContent.slice(-1);
+
+    outputScreen.textContent += operation;
+}
 
 
 window.onload = function () {
     outputScreen = document.querySelector("#result");
     clearOnEntry = true;
+    outputScreen.textContent = "0";
 };
