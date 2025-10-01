17a60acf2a9f0b3a1617b194a75514987a7cf71c
Brody Von Berge <brodyvonberge@MAC8248.lc.luther.edu>
2025-09-30 22:08:22 -0500

    todo list push

--- exercises/todo_list/js/main.js
+++ exercises/todo_list/js/main.js
@@ -10,10 +10,26 @@ var priority = ["Low", "Normal", "Important", "Critical"];
  */
 function addTask() {
     // TODO: Implement this function
-    let vals = [];
+    let titleEl = document.querySelector("#title");
+    let assignedToEl = document.querySelector("#assignedTo");
+    let priorityEl = document.querySelector("#priority");
+    let dueDateEl = document.querySelector("#dueDate");
+
+    if (!titleEl.value || !dueDateEl.value || !assignedToEl.value || !priorityEl.value) {
+        alert("All fields are required!");
+        return;
+    }
+
+    let vals = [
+        titleEl.value,
+        assignedToEl.value,
+        priorityEl.value,
+        dueDateEl.value
+    ];
 
     addRow(vals, document.querySelector("#taskList > tbody"));
 }
+
 
 /**
  * Add a new row to the table
@@ -24,11 +40,42 @@ function addTask() {
  * @param {string[]} valueList list of task attributes
  * @param {Object} parent DOM node to append to
  */
+
 function addRow(valueList, parent) {
     // TODO: Implement this function
     let row = document.createElement("tr");
+
+    let checkboxCell = document.createElement("td");
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", removeRow);
+    checkboxCell.appendChild(checkbox);
+    row.appendChild(checkboxCell);
+
+    valueList.forEach((val) => {
+        let cell = document.createElement("td");
+        cell.textContent = val;
+        row.appendChild(cell);
+    });
+
+    switch (valueList[2]) {
+        case "Critical":
+            row.classList.add("critical");
+            break;
+        case "Important":
+            row.classList.add("important");
+            break;
+        case "Normal":
+            row.classList.add("normal");
+            break;
+        case "Low":
+            row.classList.add("low");
+            break;
+    }
+
     parent.appendChild(row);
 }
+
 
 /**
  * Remove a table row corresponding to the selected checkbox after a 2 second timeout
@@ -36,14 +83,25 @@ function addRow(valueList, parent) {
  */
 function removeRow() {
     // TODO: Implement this function
+    let row = this.closest("tr"); 
+
+    if (this.checked) {
+        setTimeout(() => {
+            if (this.checked) { 
+                row.remove();
+            }
+        }, 2000);
+    }
 }
+
 
 /**
  * Remove all table rows
  * 
  */
 function selectAll() {
-
+    let tbody = document.querySelector("#taskList > tbody");
+    tbody.innerHTML = "";
 }
 
 /**
@@ -54,7 +112,17 @@ function selectAll() {
  */
 function populateSelect(selectId, sList) {
     // TODO: Implement this function
+    let select = document.getElementById(selectId);
+    select.innerHTML = "";  
+
+    sList.forEach(item => {
+        let option = document.createElement("option");
+        option.value = item;
+        option.textContent = item;
+        select.appendChild(option);
+    });
 }
+
 
 window.onload = function () {
     // Populate select elements automatically on page load
